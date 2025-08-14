c0c3fe8fabf4428c0df96fdc7e64d1d69852601f
Shun <shun@formosa1544.com>
2025-08-13 23:45:02 -0700

    Implement SQL Connector.

--- spectatordb/spectatordb.py
+++ spectatordb/spectatordb.py
@@ -1,7 +1,20 @@
+from spectatordb import sql_connector
 from spectatordb import storage
 
 
 class SpectatorDB:
 
-    def __init__(self, storage: storage.Storage):
-        pass
+    def __init__(
+        self, storage: storage.Storage, sql_connector: sql_connector.SQLiteConnector
+    ):
+        """Initialize the SpectatorDB with a storage backend.
+
+        Parameters
+        ----------
+        storage : storage.Storage
+            An instance of a storage backend that implements the Storage interface.
+        sql_connector : sql_connector.SQLiteConnector
+            An instance of the SQLiteConnector for database operations.
+        """
+        self._storage = storage
+        self._sql_connector = sql_connector
