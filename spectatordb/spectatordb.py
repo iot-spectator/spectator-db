8e1f05c849dfdbd9c07c825994019f2cee447885
Shun <shun@formosa1544.com>
2025-08-15 16:29:06 -0700

    Simplify the metadata schema.

--- spectatordb/spectatordb.py
+++ spectatordb/spectatordb.py
@@ -1,6 +1,5 @@
 import pathlib
 
-from typing import Optional
 from datetime import datetime
 
 from spectatordb import MediaType
@@ -24,24 +23,22 @@ class SpectatorDB:
         """
         self._storage = storage
         self._sql_connector = sql_connector
+        self._METADATA_TABLE = "metadata"
+        self._create_metadata_db()
 
     def _create_metadata_db(self) -> None:
-        CREATE_STATEMENT = """
-            CREATE TABLE IF NOT EXISTS metadata (
+        CREATE_STATEMENT = f"""
+            CREATE TABLE IF NOT EXISTS {self._METADATA_TABLE} (
                 name text PRIMARY KEY,
                 path text NOT NULL,
                 size integer NOT NULL,
                 media_type text NOT NULL CHECK(media_type IN ('VIDEO', 'IMAGE')),
                 format text NOT NULL,
-                description text,
-                inserted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                inserted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );"""
         self._sql_connector.execute_query(query=CREATE_STATEMENT)
 
-    def insert(
-        self, file: pathlib.Path, media_type: MediaType, description: Optional[str]
-    ) -> None:
+    def insert(self, file: pathlib.Path, media_type: MediaType) -> None:
         """Insert a media data.
 
         Parameters
@@ -51,17 +48,16 @@ class SpectatorDB:
         media_type : MediaType
             The type of the data, either VIDEO or IMAGE.
         """
-        INSERT_STATEMENT = """
-            INSERT INTO metadata (
-                name,
-                path,
-                size,
-                media_type,
-                format,
-                description,
-                inserted_at,
-                updated_at)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+        INSERT_STATEMENT = f"""
+            INSERT INTO {self._METADATA_TABLE} (
+                    name,
+                    path,
+                    size,
+                    media_type,
+                    format,
+                    inserted_at,
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?);
             """
         self._storage.save(file, mode=storage.SaveMode.COPY)
         self._sql_connector.execute_query(
@@ -72,8 +68,6 @@ class SpectatorDB:
                 file.stat().st_size,
                 media_type.value,
                 file.suffix.lstrip("."),
-                description or "",
-                datetime.now().isoformat(),
                 datetime.now().isoformat(),
             ),
         )
@@ -86,8 +80,8 @@ class SpectatorDB:
         name : str
             The name of the file to delete.
         """
-        DELETE_STATEMENT = """
-            DELETE FROM metadata WHERE name = ?;
+        DELETE_STATEMENT = f"""
+            DELETE FROM {self._METADATA_TABLE} WHERE name = ?;
             """
         self._sql_connector.execute_query(query=DELETE_STATEMENT, params=(name,))
         self._storage.delete(name=name)
